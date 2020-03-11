6a0edb2f5c66b9d34d135e94eb102ae67c63a7b9
nlund18 <49237358+anomydragon@users.noreply.github.com>
2020-03-11 23:58:48 +0100

    Normal items, like crops, have textures now

--- src/main/java/client/ClientRegisterEventsTFCE.java
+++ src/main/java/client/ClientRegisterEventsTFCE.java
@@ -3,12 +3,21 @@ package client;
 import java.util.Arrays;
 import javax.annotation.Nonnull;
 
+import net.dries007.tfc.ConfigTFC;
+import net.dries007.tfc.api.capability.food.CapabilityFood;
+import net.dries007.tfc.api.capability.food.IFood;
 import net.minecraft.block.*;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMap;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.item.*;
+import net.minecraft.item.ItemFood;
+import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -18,6 +27,8 @@ import static net.dries007.tfc.objects.blocks.agriculture.BlockCropDead.MATURE;
 import static net.dries007.tfc.objects.blocks.agriculture.BlockCropTFC.WILD;
 
 import tfcelementia.objects.blocks.BlocksTFCE;
+import tfcelementia.objects.items.ItemGemTFCE;
+import tfcelementia.util.ItemsRegistryHandler;
 import tfcelementia.TFCElementia;
 
 @SideOnly(Side.CLIENT)
@@ -28,10 +39,33 @@ public final class ClientRegisterEventsTFCE
     @SuppressWarnings("ConstantConditions")
     public static void registerModels(ModelRegistryEvent event)
     {
+		// ITEMS //
+		
+		// Simple Items
+        for (Item item : ItemsRegistryHandler.getAllSimpleItems())
+            ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName().toString()));
+		
 		for (Block block : BlocksTFCE.getAllCropBlocks())
             ModelLoader.setCustomStateMapper(block, new StateMap.Builder().ignore(WILD).build());
 
         for (Block block : BlocksTFCE.getAllDeadCropBlocks())
             ModelLoader.setCustomStateMapper(block, new StateMap.Builder().ignore(MATURE).build());
+    }
+	
+	@SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    @SuppressWarnings("deprecation")
+    public static void registerColorHandlerItems(ColorHandlerEvent.Item event)
+    {
+		ItemColors itemColors = event.getItemColors();
+		
+		itemColors.registerItemColorHandler((stack, tintIndex) -> {
+            IFood food = stack.getCapability(CapabilityFood.CAPABILITY, null);
+            if (food != null)
+            {
+                return food.isRotten() ? ConfigTFC.CLIENT.rottenFoodOverlayColor : 0xFFFFFF;
+            }
+            return 0xFFFFFF;
+        }, ForgeRegistries.ITEMS.getValuesCollection().stream().filter(x -> x instanceof ItemFood).toArray(Item[]::new));
     }
 }
