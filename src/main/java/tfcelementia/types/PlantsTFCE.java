7068a3d306b296d925a42a9be00c5c3cdafb8436
Verph <gustav-lund@outlook.com>
2020-03-12 00:04:48 +0100

    Trees

---
+++ src/main/java/tfcelementia/types/PlantsTFCE.java
@@ -0,0 +1,28 @@
+package tfcelementia.types;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import tfcelementia.TFCElementia;
+import net.dries007.tfc.api.registries.TFCRegistryEvent;
+import net.dries007.tfc.api.types.Plant;
+
+import static net.dries007.tfc.TerraFirmaCraft.MOD_ID;
+
+@SuppressWarnings("WeakerAccess")
+@Mod.EventBusSubscriber(modid = TFCElementia.MODID)
+public final class PlantsTFCE
+{
+    /**
+     * Default Plant ResourceLocations
+     */
+    public static final ResourceLocation BAMBOO = new ResourceLocation(MOD_ID, "bamboo");
+
+    @SubscribeEvent
+    public static void onPreRegisterPlant(TFCRegistryEvent.RegisterPreBlock<Plant> event)
+    {
+        event.getRegistry().registerAll(
+            new Plant(BAMBOO, Plant.PlantType.TALL_PLANT, new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, false, false, 15f, 40f, 10f, 50f, 250f, 500f, 9, 15, 20, 0D, "bamboo")
+        );
+    }
+}
